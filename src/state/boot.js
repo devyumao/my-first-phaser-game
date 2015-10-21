26bce757ab77bbf74db62870f6ffbe02bc7a924d
zhangyu38 <zhangyu38@baidu.com>
2015-10-21 16:21:35 +0800

    update

--- src/state/boot.js
+++ src/state/boot.js
@@ -11,7 +11,7 @@ DevilPace.boot = {
         // 比例设置
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; // 保持高宽比铺屏
         this.scale.pageAlignHorizontally = true;
-        this.scale.pageAlignVertically = true;
+        // this.scale.pageAlignVertically = true;
         // this.scale.setScreenSize(true); // 铺屏
 
         this.state.start('preload');
